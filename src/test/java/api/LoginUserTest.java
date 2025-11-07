19beefacc9a50ae3636ea4cbbac0f207a2b89ea1
AbdusalomH <akhodjiev@gmail.com>
2025-11-07 09:45:46 +0500

    Allure reporting - allure history fixes

--- src/test/java/api/LoginUserTest.java
+++ src/test/java/api/LoginUserTest.java
@@ -10,7 +10,8 @@ import requests.skeleton.Endpoint;
 import specs.RequestSpecs;
 import specs.ResponseSpecs;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class LoginUserTest extends BaseTest {
 
@@ -20,8 +21,6 @@ public class LoginUserTest extends BaseTest {
         UserLoginRequestModel model = UserLoginRequestModel.builder().email("admin1x@mailinator.com").password("Tester09!").build();
         UserLoginResponseModel userLoginResponseModel = new CrudeRequesters(RequestSpecs.unAuth(), Endpoint.USER_LOGIN, ResponseSpecs.requestWasSuccess()).post(model, medicalID).extract().as(UserLoginResponseModel.class);
         System.out.println(userLoginResponseModel);
-
-
     }
 
     @Test
@@ -30,5 +29,14 @@ public class LoginUserTest extends BaseTest {
         String medicalID = Config.getProperty("medicalCenterId");
         CurrentUserResponseModel currentUserResponseModel = new CrudeRequesters(RequestSpecs.adminSpecs(), Endpoint.CURRENT_USER, ResponseSpecs.requestWasSuccess()).get(medicalID).extract().as(CurrentUserResponseModel.class);
         System.out.println(currentUserResponseModel);
+
+
+        int numbes = 10;
+
+        int sum = IntStream.range(0, numbes).filter(f -> f % 2 == 0).sum();
+
+
+
+
     }
 }
